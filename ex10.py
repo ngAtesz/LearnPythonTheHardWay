e76bc4cc9261b52dae2962d1ec5907ef90057d24
ngAtesz <attila.molnar@codecool.com>
2015-08-10 12:30:11 +0200

    Exercise 10.

---
+++ ex10.py
@@ -0,0 +1,27 @@
+__author__ = 'ngAtesz'
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+
+
+def funny_code():
+    """Start a funny endless loop to print out some characters."""
+    while True:
+        for i in ["/", "-", "|", "\\", "|"]:
+            print("%s\r" % i),
+
+if __name__ == "__main__":
+    funny_code()
