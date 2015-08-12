847a977fd31e0ad419b869f5da7950c3d358cf2e
ngAtesz <attila.molnar@codecool.com>
2015-08-12 08:39:18 +0200

    Exercise 16

---
+++ ex16/ex16.py
@@ -0,0 +1,30 @@
+__author__ = 'ngAtesz'
+
+from sys import argv
+
+script, filename = argv
+
+print("We're going to erase %r." % filename)
+print("If you don't want that hit CTRL-C.")
+print("If you do want that, hit RETURN.")
+
+input("?")
+
+print("Opening the file...")
+target = open(filename, 'w')
+
+print("Truncating the file. Goodbye!")
+target.truncate()
+
+print("Now I'm going to ask you for three line.")
+
+line1 = input("line 1: ")
+line2 = input("line 2: ")
+line3 = input("line 3: ")
+
+print("I'm going to write these to the file.")
+
+target.write("{}\n{}\n{}\n".format(line1, line2, line3))
+
+print("And finally, we close it.")
+target.close()
